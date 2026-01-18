0061382e324bdcaea0e90fe6a4d7e305cf51ef65
Mojisan <gozji0801@gmail.com>
2026-01-19 04:35:40 +0700

    implemented displaying task content

--- todo-list/src/pages/todo-list-page/components/cards/TaskCard.tsx
+++ todo-list/src/pages/todo-list-page/components/cards/TaskCard.tsx
@@ -1,15 +1,24 @@
-import { Card, Title } from "@mantine/core"
+import { Card, Modal, Text, Title } from "@mantine/core"
 import type { ITask } from "../../../../interface"
+import { useDisclosure } from "@mantine/hooks"
 
 interface ITaskCardProps {
   task: ITask
 }
 
 const TaskCard = ({ task }: ITaskCardProps) => {
+  const [openedTask, closedTask] = useDisclosure(false)
+
   return (
-    <Card shadow='sm' radius='md' withBorder>
-      <Title order={4}>{task.title}</Title>
-    </Card>
+    <>
+      <Card shadow='sm' radius='md' withBorder onClick={closedTask.open}>
+        <Title order={4}>{task.title}</Title>
+      </Card>
+
+      <Modal opened={openedTask} onClose={closedTask.close} centered title={task.title}>
+        <Text>Content: {task.content}</Text>
+      </Modal>
+    </>
   )
 }
 
